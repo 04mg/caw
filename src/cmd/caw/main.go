2dd9905f73e051571abc368f28bc3f0365fb997d
04mg <manurz2121@gmail.com>
2026-07-12 14:16:47 -0300

    feat: add in-place auto-update via caw update

--- src/cmd/caw/main.go
+++ src/cmd/caw/main.go
@@ -1,21 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/04mg/caw/internal/server"
+	"github.com/04mg/caw/internal/update"
 )
 
+var Version = "dev"
+
 func main() {
-	host := "localhost"
-	if h := os.Getenv("HOST"); h != "" {
-		host = h
+	if handleSubcommands() {
+		return
 	}
 
-	port := "8080"
-	if p := os.Getenv("PORT"); p != "" {
-		port = p
-	}
+	host := envOrDefault("HOST", "localhost")
+	port := envOrDefault("PORT", "8080")
 
 	server.New().ListenAndServe(host, port)
 }
+
+func envOrDefault(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
+
+func handleSubcommands() bool {
+	if len(os.Args) < 2 {
+		return false
+	}
+
+	switch os.Args[1] {
+	case "version", "-v", "--version":
+		printVersion()
+		return true
+	case "update":
+		runUpdate()
+		return true
+	default:
+		return false
+	}
+}
+
+func printVersion() {
+	fmt.Printf("caw version %s\n", Version)
+}
+
+func runUpdate() {
+	if err := update.Run(Version); err != nil {
+		log.Fatalf("Error updating caw: %v", err)
+	}
+}
+
